d3da9c531dee008b33e73fc951eeb2a6bd8bf613
fdesi <francesco.desimone@canonical.com>
2025-07-14 02:46:14 +0200

    change fugitive keybind

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,12 +6,10 @@ map('n', '<leader>gc', ':Git commit<CR>', { desc = 'Fugitive: Commit' })
 map('n', '<leader>gac', ':Git commit --amend<CR>', { desc = 'Fugitive: Amend Previous Commit' })
 map('n', '<leader>gP', ':Git push<CR>', { desc = 'Fugitive: Push' })
 map('n', '<leader>gp', ':Git pull<CR>', { desc = 'Fugitive: Pull' })
-map('n', '<leader>gb', ':Git blame<CR>', { desc = 'Fugitive: Blame Current File' })
+map('n', '<leader>gB', ':Git blame<CR>', { desc = 'Fugitive: Blame Current File' })
 map('n', '<leader>gr', ':Gread<CR>', { desc = 'Fugitive: Revert Buffer to HEAD (Gread)' })
-map('n', '<leader>gg', ':Ggrep ', { noremap = true, desc = 'Fugitive: Grep (git grep)' })
-map('n', '<leader>go', ':GBrowse<CR>', { desc = 'Fugitive: Open File on Hosting Provider (GBrowse)' })
-map('v', '<leader>go', ':GBrowse<CR>', { desc = 'Fugitive: Open Selection on Hosting Provider (GBrowse)' })
+-- map('n', '<leader>gg', ':Ggrep ', { noremap = true, desc = 'Fugitive: Grep (git grep)' })
 return {
   'tpope/vim-fugitive',
-  dependencies = { 'tpope/vim-rhubarb' },
+  -- dependencies = { 'tpope/vim-rhubarb' },
 }
