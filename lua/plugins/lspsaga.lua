51d6100c30d34068429d0e05af180cb109647270
fdesi <francesco.desimone@canonical.com>
2025-07-14 02:45:20 +0200

    changing keymaps; removing telescope

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,11 +1,10 @@
-vim.keymap.set('n', '<c-/>', ':Lspsaga term_toggle<CR>', { desc = 'Toggle terminal' })
 vim.keymap.set('n', '<leader>li', ':Lspsaga incoming_calls<CR>', { desc = 'Incoming calls' })
 vim.keymap.set('n', '<leader>lo', ':Lspsaga outgoing_calls<CR>', { desc = 'Outgoing calls' })
 vim.keymap.set('n', '<leader>la', ':Lspsaga code_action<CR>', { desc = 'Code action' })
 vim.keymap.set('n', '<leader>lf', ':Lspsaga finder<CR>', { desc = 'Finder' })
 vim.keymap.set('n', '<leader>ls', ':Lspsaga outline<CR>', { desc = 'symbol outline' })
 vim.keymap.set('n', '<leader>ld', ':Lspsaga peek_definition<CR>', { desc = 'Peek definition' })
-vim.keymap.set('n', '<leader>lr', ':Lspsaga rename<CR>', { desc = 'Rename' })
+vim.keymap.set('n', '<leader>rr', ':Lspsaga rename<CR>', { desc = 'Rename' })
 return {
   'nvimdev/lspsaga.nvim',
   config = function()
@@ -13,6 +12,6 @@ return {
   end,
   dependencies = {
     'nvim-treesitter/nvim-treesitter', -- optional
-    'nvim-tree/nvim-web-devicons', -- optional
+    -- 'nvim-tree/nvim-web-devicons', -- optional
   },
 }
