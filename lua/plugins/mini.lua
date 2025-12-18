7b42e3de65262b644ee4f5d4b6d6e5045d0c0f35
fdesi <francesco.desimone@canonical.com>
2025-12-18 20:41:58 +0100

    update plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,5 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
+  'nvim-mini/mini.nvim',
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return { -- Highlight, edit, and navigate code
   dependencies = { 'OXY2DEV/markview.nvim' },
   priority = 50,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
