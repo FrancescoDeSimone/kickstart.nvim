e5aa1f0d1947c696e360bd0a8f3fe4ff7cd86f2e
fdesi <francesco.desimone@canonical.com>
2026-01-09 18:47:41 +0100

    add oil plugins

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -45,5 +45,14 @@ return {
       -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
       -- lazy = false,
     },
+    {
+      'benomahony/oil-git.nvim',
+      dependencies = { 'stevearc/oil.nvim' },
+    },
+    {
+      'JezerM/oil-lsp-diagnostics.nvim',
+      dependencies = { 'stevearc/oil.nvim' },
+      opts = {},
+    },
   },
 }
