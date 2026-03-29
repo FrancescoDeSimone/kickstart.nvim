9cb2270181c6016b40a49520b8bdac33d67682ea
fdesi <francesco.desimone@canonical.com>
2026-03-30 00:23:46 +0200

    improve which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,30 +2,48 @@ return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   opts = {
+    preset = 'modern',
     delay = 0,
-    icons = { mappings = true },
+    icons = {
+      mappings = true,
+      group = ' ',
+      separator = ' ',
+      colors = true,
+    },
     spec = {
       -- Programming
-      { '<leader>p', group = '[P]rogramming' },
+      { '<leader>p', group = '[P]rogramming', icon = { icon = ' ', color = 'orange' } },
       { '<leader>pa', desc = 'Code [A]ction' },
       { '<leader>pf', desc = 'Format File' },
 
-      -- Debug
-      { '<leader>D', group = '[D]ebug' },
-
       -- Run/Rename
-      { '<leader>pr', group = '[R]un / Rename' },
+      { '<leader>pr', group = '[R]un / Rename', icon = { icon = ' ', color = 'blue' } },
       { '<leader>prr', desc = 'Run Current File' },
       { '<leader>prn', desc = 'Rename Symbol' },
 
       -- Environment & Packages
-      { '<leader>pv', group = '[V]env / Packages' },
+      { '<leader>pv', group = '[V]env / Packages', icon = { icon = ' ', color = 'green' } },
       { '<leader>pve', desc = 'Manage Venv' },
       { '<leader>pva', desc = 'Add Package' },
       { '<leader>pvc', desc = 'Sync Packages' },
 
+      -- C++ Reference
+      { '<leader>c', group = '[C]++ Reference', icon = { icon = '󰙲 ', color = 'blue' } },
+      { '<leader>cm', desc = 'Cppman: Word Under Cursor' },
+      { '<leader>cc', desc = 'Cppman: Search' },
+
+      -- Debug
+      { '<leader>D', group = '[D]ebug', icon = { icon = '󰃤 ', color = 'red' } },
+
+      -- Folds
+      { '<leader>f', group = '[F]olds', icon = { icon = ' ', color = 'yellow' } },
+      { '<leader>fo', desc = 'Toggle Fold' },
+      { '<leader>fO', desc = 'Toggle All Folds' },
+      { '<leader>fr', desc = 'Open All Folds' },
+      { '<leader>fm', desc = 'Close All Folds' },
+
       -- Search
-      { '<leader>s', group = '[S]earch' },
+      { '<leader>s', group = '[S]earch', icon = { icon = ' ', color = 'green' } },
       { '<leader>sf', desc = 'Find Files' },
       { '<leader>s/', desc = 'Live Grep' },
       { '<leader>sB', desc = 'Grep Buffers' },
@@ -36,13 +54,14 @@ return {
       { '<leader>sm', desc = 'Marks' },
       { '<leader>sM', desc = 'Man Pages' },
       { '<leader>sp', desc = 'Projects' },
+      { '<leader>sr', desc = 'Replace Word' },
+      { '<leader>sR', desc = 'Search/Replace' },
       { '<leader>ss', desc = 'LSP Symbols' },
       { '<leader>sS', desc = 'Workspace Symbols' },
       { '<leader>su', desc = 'Undo History' },
-      { '<leader>sR', desc = 'Resume Picker' },
 
       -- Git
-      { '<leader>g', group = '[G]it' },
+      { '<leader>g', group = '[G]it', icon = { icon = ' ', color = 'orange' } },
       { '<leader>gg', desc = 'Git Status' },
       { '<leader>gc', desc = 'Git Commit' },
       { '<leader>gp', desc = 'Git Pull' },
@@ -52,7 +71,7 @@ return {
       { '<leader>gd', desc = 'Diffview Open' },
       { '<leader>gD', desc = 'Diffview Close' },
       { '<leader>gB', desc = 'Git Browse' },
-      { '<leader>gh', group = 'Git [H]unks' },
+      { '<leader>gh', group = 'Git [H]unks', icon = { icon = '󰊢 ', color = 'orange' } },
       { '<leader>ghs', desc = 'Stage Hunk' },
       { '<leader>ghr', desc = 'Reset Hunk' },
       { '<leader>ghS', desc = 'Stage Buffer' },
@@ -66,41 +85,44 @@ return {
       { '<leader>ghF', desc = 'File History (All)' },
 
       -- Explorer
-      { '<leader>e', group = '[E]xplorer' },
-      { '<leader>e', desc = 'File Explorer' },
-      { '<leader>-', desc = 'File Explorer (Split)' },
+      { '<leader>e', desc = 'File Explorer', icon = { icon = '󰙅 ', color = 'cyan' } },
+      { '<leader>-', desc = 'File Explorer (Split)', icon = { icon = '󰙅 ', color = 'cyan' } },
 
       -- Buffers
-      { '<leader>b', group = '[B]uffers' },
-      { '<leader>b', desc = 'Buffers' },
-      { '<leader>q', desc = 'Delete Buffer' },
+      { '<leader>b', desc = 'Buffers', icon = { icon = '󰈔 ', color = 'cyan' } },
+      { '<leader>q', desc = 'Delete Buffer', icon = { icon = '󰅖 ', color = 'red' } },
 
       -- Diagnostics
-      { '<leader>x', group = '[X] Diagnostics' },
+      { '<leader>x', group = '[X] Diagnostics', icon = { icon = '󱖫 ', color = 'green' } },
       { '<leader>xD', desc = 'Diagnostics' },
       { '<leader>xd', desc = 'Buffer Diagnostics' },
 
       -- Replace/Refactor
-      { '<leader>r', group = '[R]eplace/Refactor' },
+      { '<leader>r', group = '[R]eplace/Refactor', icon = { icon = ' ', color = 'cyan' } },
       { '<leader>rw', desc = 'Wrap Line at 80 Chars' },
-      { '<leader>sr', desc = 'Replace Word' },
-      { '<leader>sR', desc = 'Search/Replace' },
       { '<leader>rf', desc = 'Format Buffer' },
       { '<leader>rR', desc = 'Rename File' },
 
-      -- Utilities
-      { '<leader>t', group = '[T]ools' },
-      { '<leader>z', desc = 'Toggle Zoom' },
-      { '<leader>Z', desc = 'Toggle Zen Mode' },
-      { '<leader>.', desc = 'Scratch Buffer' },
-      { '<leader>S', desc = 'Select Scratch' },
-      { '<leader>N', desc = 'Neovim News' },
-      { '<leader>y', desc = 'Registers' },
-      { '<leader>/', desc = 'Buffer Lines' },
-      { '<leader>:', desc = 'Command History' },
-      { '<leader>n', desc = 'Notification History' },
+      -- UI Toggles
+      { '<leader>u', group = '[U]I', icon = { icon = '󰙵 ', color = 'cyan' } },
+
+      -- Tools
+      { '<leader>t', group = '[T]ools', icon = { icon = ' ', color = 'purple' } },
       { '<leader>tR', desc = 'Resume Picker' },
+
+      -- Standalone keys with explicit icons
+      { '<leader>z', desc = 'Toggle Zoom', icon = { icon = '󰁌 ', color = 'blue' } },
+      { '<leader>Z', desc = 'Toggle Zen Mode', icon = { icon = '󱅻 ', color = 'cyan' } },
+      { '<leader>.', desc = 'Scratch Buffer', icon = { icon = '󰏫 ', color = 'yellow' } },
+      { '<leader>S', group = '[S]ession', icon = { icon = ' ', color = 'azure' } },
+      { '<leader>Ss', desc = 'Save Session' },
+      { '<leader>Sl', desc = 'Load Session' },
+      { '<leader>N', desc = 'Neovim News', icon = { icon = '󰎕 ', color = 'green' } },
+      { '<leader>y', desc = 'Registers', icon = { icon = '󱓥 ', color = 'yellow' } },
+      { '<leader>/', desc = 'Buffer Lines', icon = { icon = ' ', color = 'green' } },
+      { '<leader>:', desc = 'Command History', icon = { icon = ' ', color = 'purple' } },
+      { '<leader>n', desc = 'Notification History', icon = { icon = '󰵅 ', color = 'blue' } },
+      { '<leader><leader>', desc = 'Peep', icon = { icon = '󰈈 ', color = 'yellow' } },
     },
   },
 }
-
